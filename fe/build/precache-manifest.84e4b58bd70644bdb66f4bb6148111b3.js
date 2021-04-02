11e52e48490a041ac78b7adc23cee226914c9262
Aditya Kamat <kamat.a@northeastern.edu>
2021-04-01 23:09:41 -0400

    twitter project added

---
+++ fe/build/precache-manifest.84e4b58bd70644bdb66f4bb6148111b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15c1c1ed02bd90b5b0b47e14ecb2a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "717ef98bca3a48437a69",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "70b80edd643775e66359",
+    "url": "/static/js/2.4eb74f49.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.4eb74f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717ef98bca3a48437a69",
+    "url": "/static/js/main.8b1699ae.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
